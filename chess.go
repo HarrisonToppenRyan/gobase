b32f11a2042d6f35baef17761de789c5ff3eaf22
WilliamDann <wcdann1@gmail.com>
2020-08-10 19:30:17 -0700

    added parsing of FEN strings

--- chess.go
+++ chess.go
@@ -6,8 +6,32 @@ type ChessBoard struct {
 }
 
 // create a fen string from a chess position
-func CreateFromFEN(fen string) ChessBoard {
-	return ChessBoard{pieces: [64]byte{}}
+func CreateFromFEN(fen string) (board ChessBoard) {
+	board = ChessBoard{pieces: [64]byte{}}
+
+	var position int = 0
+	for i := 0; i < len(fen); i++ {
+		if fen[i] == '/' {
+			continue
+		}
+
+		// check if fen character is a number
+		if fen[i] < 57 && fen[i] > 48 {
+			// get blank square number
+			num := int(fen[i]) - 48
+
+			// add blank squares
+			for j := 0; j < num; j++ {
+				board.pieces[position] = 0
+				position++
+			}
+		} else {
+			board.pieces[position] = fen[i]
+			position++
+		}
+	}
+
+	return board
 }
 
 // get the string form of a chessboard
@@ -20,7 +44,7 @@ func String(board ChessBoard) (str string) {
 		if board.pieces[i] == 0 {
 			str += "_ "
 		} else {
-			str += string(board.pieces[i])
+			str += string(string(board.pieces[i]) + " ")
 		}
 	}
 	return str
--- main.go
+++ main.go
@@ -3,6 +3,6 @@ package main
 import "fmt"
 
 func main() {
-	board := CreateFromFEN("0/0/0/0/0/0/0/0")
+	board := CreateFromFEN("r1b1k2r/pp3ppp/2pp4/8/2P5/3NPnP1/PPK2P1P/R4B1R")
 	fmt.Println(String(board))
 }
