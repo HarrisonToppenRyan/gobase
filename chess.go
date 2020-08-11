08f0abe268dfd6c65afb2268b5b13c13cd8c7b81
WilliamDann <wcdann1@gmail.com>
2020-08-10 18:58:12 -0700

    added String to board

--- chess.go
+++ chess.go
@@ -9,3 +9,19 @@ type ChessBoard struct {
 func CreateFromFEN(fen string) ChessBoard {
 	return ChessBoard{pieces: [64]byte{}}
 }
+
+// get the string form of a chessboard
+func String(board ChessBoard) (str string) {
+	for i := 0; i < len(board.pieces); i++ {
+		if i%8 == 0 && i != 0 {
+			str += "\n"
+		}
+
+		if board.pieces[i] == 0 {
+			str += "_ "
+		} else {
+			str += string(board.pieces[i])
+		}
+	}
+	return str
+}
--- main.go
+++ main.go
@@ -4,15 +4,5 @@ import "fmt"
 
 func main() {
 	board := CreateFromFEN("0/0/0/0/0/0/0/0")
-	for i := 0; i < len(board.pieces); i++ {
-		if i%8 == 0 && i != 0 {
-			fmt.Print("\n")
-		}
-
-		if board.pieces[i] == 0 {
-			fmt.Print("_" + " ")
-		} else {
-			fmt.Print(string(board.pieces[i]) + " ")
-		}
-	}
+	fmt.Println(String(board))
 }
