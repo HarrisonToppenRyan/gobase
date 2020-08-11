8e3f3904b4ff92f5ea14adf07c1b74706ffe3775
WilliamDann <wcdann1@gmail.com>
2020-08-10 18:52:29 -0700

    added blank board

--- chess.go
+++ chess.go
@@ -2,5 +2,10 @@ package main
 
 // store a chessboard position
 type ChessBoard struct {
-	pieces [][]byte
+	pieces [64]byte
+}
+
+// create a fen string from a chess position
+func CreateFromFEN(fen string) ChessBoard {
+	return ChessBoard{pieces: [64]byte{}}
 }
--- main.go
+++ main.go
@@ -1,6 +1,18 @@
 package main
 
+import "fmt"
+
 func main() {
-	//board := CreateFromFEN("0/0/0/0/0/0/0/0")
-	//fmt.Println(board.pieces[0])
+	board := CreateFromFEN("0/0/0/0/0/0/0/0")
+	for i := 0; i < len(board.pieces); i++ {
+		if i%8 == 0 && i != 0 {
+			fmt.Print("\n")
+		}
+
+		if board.pieces[i] == 0 {
+			fmt.Print("_" + " ")
+		} else {
+			fmt.Print(string(board.pieces[i]) + " ")
+		}
+	}
 }
